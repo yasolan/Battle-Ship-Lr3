fb64e31f2cdd83c46f9f7fd2867b61af8adaa422
yasolan <maslovskijartem1@gmail.com>
2025-05-15 15:25:54 +0300

    Add files via upload

--- bullet.py
+++ bullet.py
@@ -3,9 +3,10 @@ from pygame.sprite import Sprite
 
 class Bullet(Sprite):
     def __init__(self, ai_settings, screen, ship):
-        super().__init__()
+        super(Bullet, self).__init__()
         self.screen = screen
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+            ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
         self.y = float(self.rect.y)
