a1cba672ed6cc6f7033e92a28d47c47e62e9a2c5
yasolan <maslovskijartem1@gmail.com>
2025-05-15 14:40:29 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
-import pygame
-
 class Settings():
     def __init__(self):
         self.screen_width = 1200
-        self.screen_hight = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        self.ship_speed_factor = 1.5
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
